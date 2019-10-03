1b596351355b525798f81769cc9729636c83ae18
Geektanic <drhodes@wvup.edu>
2019-10-03 10:16:38 -0400

    Learning debugging

---
+++ 7.2-squashing-bugs.py
@@ -0,0 +1,10 @@
+# Book has us start with some buggy code
+
+def add_underscores(word):
+    new_word = "_"
+    for i in range(0, len(word)):
+        new_word = word[i] + "_"
+    return new_word
+
+phrase = "hello"
+print(add_underscores(phrase))
