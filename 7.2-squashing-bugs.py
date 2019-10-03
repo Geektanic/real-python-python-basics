3528dfa5da5f0e355e47abe4ba00ac5ed737d69d
Doug Rhodes <drhodes@wvup.edu>
2019-10-03 10:37:50 -0400

    Synching

--- 7.2-squashing-bugs.py
+++ 7.2-squashing-bugs.py
@@ -3,8 +3,10 @@
 def add_underscores(word):
     new_word = "_"
     for i in range(0, len(word)):
-        new_word = word[i] + "_"
+        new_word = new_word + word[i] + "_"
     return new_word
 
 phrase = "hello"
 print(add_underscores(phrase))
+
+# The above is my fixed code after watching it run in debug mode
