415fc4232dd4c273909fe3a18ad90f295ba5d184
Geektanic <doug.rhodes@gmail.com>
2019-10-05 22:47:29 -0400

    Synch

--- 8.3-control-the-flow.py
+++ 8.3-control-the-flow.py
@@ -1,20 +1,29 @@
 # Who wins at the sports game?
+# The book example was just a fairly simple example using if, elif, and else.
+# I've modified it to the below code using more of what the book has taught up to this point.
+# The major change is setting up functions.  That also changes the behavior of the ending if/else,
+# as it was an if/elif/else statement before.
 
 def select_sport():
-    test_sport = "invalid"
-    while test_sport != "basketball" or "golf":
-        test_sport = input("What game are you playing?\nOptions are basketball or golf: ")
-    return sport
+    test_sport = input("What game are you playing?\nOptions are basketball or golf: ")
+    if test_sport.lower() == "basketball" or test_sport.lower() == "golf":
+        return test_sport
+    else:
+        select_sport()
+
+def score_input(player_number):
+    test_score = input(f"Enter Player {player_number} score: ")
+    return test_score
 
 sport = select_sport()
 
-p1_score = input("Enter Player 1 score: ")
-p2_score = input("Enter Player 2 score: ")
+p1_score = score_input(1)
+p2_score = score_input(2)
 
 if p1_score == p2_score:
     print("The game was a tie/draw.")
 
-elif sport.lower() == "basketball" or sport.lower() == "golf":
+else:
     p1_wins_basketball = sport == "basketball" and p1_score > p2_score
     p1_wins_golf = sport == "golf" and p1_score < p2_score
     p1_wins = p1_wins_basketball or p1_wins_golf
@@ -24,5 +33,14 @@ elif sport.lower() == "basketball" or sport.lower() == "golf":
     else:
         print("Player 2 wins.")
 
+
+# Review exercise 1
+
+word = input("Please enter one word: ")
+
+if len(word) < 5:
+    print("That word is less than five letters long.")
+elif len(word) > 5:
+    print("That word is greater than five letters long.")
 else:
-    print("Unknown sport.")
+    print("That word is five letters long.")
