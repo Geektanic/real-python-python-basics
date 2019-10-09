38dfda2d1c125f68ed79365577fe145b9db582e3
Geektanic <doug.rhodes@gmail.com>
2019-10-09 00:04:37 -0400

    Synch

---
+++ 9.1-tuples.py
@@ -0,0 +1,31 @@
+# Review exercise 1
+
+cardinal_numbers = ("first", "second", "third")
+
+# Review exercise 2
+
+print(cardinal_numbers[1])
+
+# Review exercise 3
+
+position1, position2, position3 = cardinal_numbers
+
+print(position1)
+print(position2)
+print(position3)
+
+# Review exercise 4
+
+my_name = tuple("Douglas")
+print(my_name)
+
+# Review exercise 5
+
+y = "x" in my_name
+print(y)
+
+# Review exercise 6
+# I know there's a better way, but I'm not seeing it yet
+
+some_of_my_name = (my_name[1], my_name[2], my_name[3], my_name[4], my_name[5], my_name[6])
+print(some_of_my_name)
