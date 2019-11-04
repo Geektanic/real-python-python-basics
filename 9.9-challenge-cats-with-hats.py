554c757c7edc9f084310a2bfd9a23c071057d574
Geektanic <doug.rhodes@gmail.com>
2019-11-03 22:53:21 -0500

    Synch.

--- 9.9-challenge-cats-with-hats.py
+++ 9.9-challenge-cats-with-hats.py
@@ -9,6 +9,8 @@ for n in range(0, 100):
 
 # The real work begins, in which each time I go around the ring of cats, I either place a hat if no hat
 # is on a cat, or I take a hat off if the cat has a hat.
+# The instructions only call for printing out the list of cats with hats after the last pass.
+# I have played around with a few different ways to output the ring of cats.
 
 for the_pass in range(1, 101):
     for cat in range(len(cat_ring)):
@@ -18,5 +20,32 @@ for the_pass in range(1, 101):
             else:
                 cat_ring[cat] = f"Cat {cat+1} has no hat. :("
 
+print("")
+print("The full ring of cats looks like this:")
+
 for output in range(len(cat_ring)):
     print(cat_ring[output])
+
+print("")
+print("Only the cats below have hats:")
+
+for new_out in range(len(cat_ring)):
+    if "has a hat" in cat_ring[new_out]:
+        print(cat_ring[new_out])
+
+cats_with_hats = []
+
+for summary_out in range(0, 100):
+    if "has a hat" in cat_ring[summary_out]:
+        cats_with_hats.append(f"{summary_out + 1}")
+
+summary = ""
+
+for new_summary_out in range(0, 100):
+    if "has a hat" in cat_ring[new_summary_out]:
+        summary = summary + f"{new_summary_out+ 1}, "
+
+summary = summary[0:len(summary)-2]
+
+print("")
+print(f"Finally, to summarize, cats {summary} have hats! :)))")
