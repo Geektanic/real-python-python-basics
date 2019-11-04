19a42b5baea0288fa245dd83d39a952faeb622e5
Geektanic <drhodes@wvup.edu>
2019-11-04 10:04:14 -0500

    Cleaning up.

--- 04.8-finding-strings.py
+++ 04.8-finding-strings.py
@@ -1,24 +1,27 @@
-"""phrase = "the surprise is somewhere in here"
-print(phrase.find("surprise"))"""
+# Code while going through the chapter.
+
+phrase = "the surprise is somewhere in here"
+print(phrase.find("surprise"))
 
 
-"""phrase = input("Please enter a phrase: ")
+phrase = input("Please enter a phrase: ")
 word = input("Please enter a word to search for: ")
 position = phrase.find(word) + 1
-print("\nYour word first appears at position #" + str(position))"""
+print("\nYour word first appears at position #" + str(position))
 
 
 # Review exercises
+
 # Review exercise #1
-"""
+
 print("AAA".find("a"))
-"""
+
 # Review exercise #2
-"""
+
 n = 2.0
 searchThis = input("Getting string: ")
 print(searchThis.find(str(n)))
-"""
+
 #Review exercise #3
 
 searchThis = input("Getting a phrase: ")
