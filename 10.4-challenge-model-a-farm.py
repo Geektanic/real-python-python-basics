e2fe387317cbcca034b72c85a427de703b956ca5
Geektanic <drhodes@wvup.edu>
2020-01-30 12:34:18 -0500

    Exiting hibernation...time to start this again.

--- 10.4-challenge-model-a-farm.py
+++ 10.4-challenge-model-a-farm.py
@@ -46,3 +46,43 @@ class Chicken(Animal):
 
     def location(self, location):
         return f"{self.name} is in the {self.location}."
+
+
+# Time to make up some kind of scenario(s) that might utilize the above stuff.
+# Let's see what happens!
+
+animals = ["Cow", "Horse", "Pig", "Chicken"]
+animal = "Not here"
+
+print("Welcome to the farm!")
+print("We accept cows, horses, pigs, and chickens.")
+
+while animal not in animals:
+    animal = input("Which type of animal would you like to bring to the farm? ").lower().capitalize()
+    if animal in animals:
+        break
+    print("We don't accept that type of animal. Please come back with a cow, horse, pig, or chicken.")
+
+new_name = input(f"What is this {animal}'s name? ").lower().capitalize()
+new_sex = input(f"What is this {new_name}'s sex? ")
+new_age = input(f"What is this {new_name}'s age? ")
+new_num_legs = input(f"How many legs does {new_name} have? ")
+new_color = input(f"What single color would use to describe {new_name}? ").lower()
+new_spots = input(f"Does {new_name} have spots? ").lower()
+new_dairy_or_slaughter = input(f"Is {new_name} for dairy or slaughter? ").lower()
+
+the_animal = Cow(new_name, new_sex, new_age, new_num_legs, new_color, new_spots, new_dairy_or_slaughter)
+
+print("----------------------------------------------------------------------------")
+print(f"To review, the animal you brought today, {the_animal.name}, is a {the_animal.age} year old, {the_animal.sex.lower()} {animal.lower()}.")
+
+if the_animal.spots == "yes":
+    print(f"{the_animal.name} is also a {the_animal.color}, {the_animal.num_legs} legged, {the_animal.dairy_or_slaughter} cow with spots.")
+else:
+    print(f"{the_animal.name} is also a {the_animal.color}, {the_animal.num_legs} legged, {the_animal.dairy_or_slaughter} cow without spots.")
+
+print("----------------------------------------------------------------------------")
+print("---------------------------TWO WEEKS LATER----------------------------------")
+print("----------------------------------------------------------------------------")
+
+print(the_animal.ready(the_animal.dairy_or_slaughter))
